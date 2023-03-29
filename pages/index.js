31cc4ce602e0bcc1d93bd56288dba008e66229c8
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-03-29 17:46:18 +0200

    add index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
+import ArtPiece from "../components/ArtPieces";
+import Spotlight from "../components/Spotlight";
+import useSWR from "swr";
+
 export default function HomePage() {
+  const { data, isLoading } = useSWR("https://example-apis.vercel.app/api/art");
+
+  if (isLoading) return <h2>Is loading, no art pieces yet!</h2>;
+
+  const artData = data;
+  const spotlightArtData =
+    artData[Math.floor(Math.random() * (artData.length - 0) + 0)];
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <Spotlight data={spotlightArtData} />
+      <ArtPiece data={artData} />
     </div>
   );
 }
